fb974b26939c3d478c436f10932a809e9e44ed9a
ThePrimeagen <samueljamieson02@gmail.com>
2025-01-01 10:28:51 +0000

    make base html, base py with imports of flask and flask forms

---
+++ base.py
@@ -0,0 +1,5 @@
+from flask import Flask,redirect,session,render_template,url_for
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField,StringField,IntegerField,SubmitField,RadioField,BooleanField
+
+
