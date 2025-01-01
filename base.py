0a388c5e9b842895a4e36a0e731a24e8a011e69e
ThePrimeagen <samueljamieson02@gmail.com>
2025-01-01 10:39:14 +0000

    made fmap choice

--- base.py
+++ base.py
@@ -2,4 +2,9 @@ from flask import Flask,redirect,session,render_template,url_for
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField,StringField,IntegerField,SubmitField,RadioField,BooleanField
 
+app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'key'
+
+class tarkov (FlaskForm):
+    fmap = RadioField("favourite map:  ",choices=[("woods","Woods"),("customs","Customs"),("labs","Labs"),("interchange","Interchange"),("light","Lighthouse"),("Ground","Ground Zero"),("reserve","Reserve"),("Streets","Streets"),("shorline","Shortline")])
